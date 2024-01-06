5a9b9c91dd42f4066e8b856558dbaa7430624826
Andres Canon <andres.canon@usegale.cloud>
2024-01-06 04:36:00 -0500

    feat: [PWA-003] Dark Mode Hook Added - TopCards Draft Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
 
-import "./App.css";
-import LeftForm from "./components/organisms/LeftForm";
+import "./App.scss";
+import Home from "./components/pages/home";
+import useThemeMode from "./hooks/useThemeMode";
+import SwitchDarkMode from "./components/molecules/SwitchDarkMode";
 
 function App() {
+  const { theme, toggleMode } = useThemeMode();
+
+  const handleToggleMode = () => {
+    toggleMode();
+  };
+
   return (
-    <div className="App">
-      <LeftForm />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home />
+      <SwitchDarkMode
+        theme={theme}
+        checked={theme.palette.mode === "dark"}
+        onChange={handleToggleMode}
+      />
+    </ThemeProvider>
   );
 }
 
