e99d3e58b9904e9a5a98b545509e39e4536f6a10
Andres Canon <andres.canon@usegale.cloud>
2024-01-07 22:02:09 -0500

    feat: [PWA-005] Dark Mode Context Refactored

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import CustomThemeProvider from "./hooks/useThemeContext/provider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomThemeProvider>
+      <App />
+    </CustomThemeProvider>
   </React.StrictMode>
 );
 
